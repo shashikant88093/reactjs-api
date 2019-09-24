329d92fe442af56923132f4484f92b1cbe12ee0f
shashikant88093 <shashi94goswami@gmail.com>
2019-09-25 00:15:12 +0530

    Updates

---
+++ precache-manifest.556ffecfea5ec9b2d13fc92c9b6ed28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a6dc4d0c32322155e7c4859e33de51",
+    "url": "/shashikant88093/reactjs-api.git/index.html"
+  },
+  {
+    "revision": "4c69ae3cffab7aab1315",
+    "url": "/shashikant88093/reactjs-api.git/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4c69ae3cffab7aab1315",
+    "url": "/shashikant88093/reactjs-api.git/static/js/2.2886390c.chunk.js"
+  },
+  {
+    "revision": "85f446fae711953139cf",
+    "url": "/shashikant88093/reactjs-api.git/static/js/main.3d6cbfbb.chunk.js"
+  },
+  {
+    "revision": "edfd98f3eca0c7e22cdd",
+    "url": "/shashikant88093/reactjs-api.git/static/js/runtime~main.3c3f17db.js"
+  }
+]);
